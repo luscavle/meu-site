01d11f6b03dde4684a3c312f39438d59224aa686
luscavle <lucsvale@gmail.com>
2026-05-16 16:08:39 +0100

    Update content.js

--- lucasvale-site-vercel-final/src/data/content.js
+++ lucasvale-site-vercel-final/src/data/content.js
@@ -60,6 +60,7 @@ export const publications = [
     year: "2026",
     description:
       "Discussão sobre as possibilidades e limites da Realidade Virtual para reconfigurar o passado como experiência em contextos educacionais.",
+     link: "https://orcid.org/0009-0003-3261-8139#:~:text=10.18265/2447%2D9187a2026id9240"
   },
   {
     type: "Revisão Sistemática",
